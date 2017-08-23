28817464f1ea31a6a4c84869efe7a3f4908330ac
Cillié Malan <cilliemalan@gmail.com>
2017-08-23 15:11:33 +0200

    using opn in development

--- server/index.js
+++ server/index.js
@@ -1,19 +1,27 @@
 const express = require('express');
 const webpack = require('webpack');
 const webpackconfig = require('../webpack.config');
-const webpackMiddleware = require("webpack-dev-middleware");
-const webpackHotMiddleware = require("webpack-hot-middleware");
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const opn = require('opn');
+
+const development = process.env.NODE_ENV != 'production';
+const port = 3000;
 const app = express();
 
 app.use(express.static('www'));
 
 const webpackCompiler = webpack(webpackconfig);
-const wpmw = webpackMiddleware(webpackCompiler,{});
+const wpmw = webpackMiddleware(webpackCompiler, {});
 app.use(wpmw);
 
 const wphmw = webpackHotMiddleware(webpackCompiler);
 app.use(wphmw);
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!')
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+
+    if (development) {
+        opn(`http://127.0.0.1:${port}/`);
+    }
 });
