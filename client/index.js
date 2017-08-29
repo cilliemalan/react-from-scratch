c80daf5e45bbd34cc125616df1dacb3caf0caedf
Cillié Malan <cilliemalan@gmail.com>
2017-08-29 10:56:46 +0200

    Extracted App component

--- client/index.js
+++ client/index.js
@@ -1,10 +1,7 @@
-import reactlogo from './content/react.svg';
 import './styles/main.scss';
 import ReactDOM from 'react-dom';
 import React from 'react';
-import {Greeter} from './components/Greeter';
+import { App } from './App'
 
 const approot = document.getElementById('react-app');
-ReactDOM.render(
-    <Greeter src={reactlogo} message={"Hello World"} />,
-    approot);
+ReactDOM.render(<App />, approot);
