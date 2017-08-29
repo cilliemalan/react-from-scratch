bbc9a88ef0e78cbcff4e950baab5e476a34c1fd4
Cillié Malan <cilliemalan@gmail.com>
2017-08-29 16:54:55 +0200

    Moved HashRouter to App component

--- client/App.jsx
+++ client/App.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom'
+import { Route, Link, HashRouter } from 'react-router-dom'
 import { List } from './components/List';
 import { Login } from './components/Login';
 import { Preferences } from './components/Preferences';
 import { Reminder } from './components/Reminder';
 
-export function App() {
-    return <div>
-        <nav>
-            <Link to="/">Home</Link>
-            <Link to="/login">"Login"</Link>
-            <Link to="/preferences">Preferences</Link>
-            <Link to="/reminder">Reminder</Link>
-        </nav>
-        <Route exact path="/" component={List}/>
-        <Route exact path="/login" component={Login}/>
-        <Route path="/preferences" component={Preferences}/>
-        <Route path="/reminder" component={Reminder}/>
-    </div>;
+export function App(props) {
+    return <HashRouter>
+        <div>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/login">"Login"</Link>
+                <Link to="/preferences">Preferences</Link>
+                <Link to="/reminder">Reminder</Link>
+            </nav>
+            <Route exact path="/" component={List} />
+            <Route exact path="/login" component={Login} />
+            <Route path="/preferences" component={Preferences} />
+            <Route path="/reminder" component={Reminder} />
+        </div>
+    </HashRouter>;
 }
--- client/index.js
+++ client/index.js
@@ -1,16 +1,13 @@
 import './styles/main.scss';
 import ReactDOM from 'react-dom';
 import React from 'react';
-import { HashRouter } from 'react-router-dom';
 import { AppContainer } from 'react-hot-loader';
 import { App } from './App'
 
 const approot = document.getElementById('react-app');
 ReactDOM.render(
     <AppContainer>
-        <HashRouter>
-            <App />
-        </HashRouter>
+        <App />
     </AppContainer>,
     approot);
 
